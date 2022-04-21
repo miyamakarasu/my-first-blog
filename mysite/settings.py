641f41ee667ce253756f6c0e71f9ebaef8988ca5
Your Name <you@example.com>
2022-04-21 21:44:19 +0900

    whitenoise psycopg2 version change requirements settings wsgi

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #ここに書き足す。
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
